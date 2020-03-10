8dcfd2f2d2b3e4b6aeeb7bba3016e1a26bb2f20d
Nicole Gathany <nicole.gathany@gmail.com>
2020-03-10 12:05:30 -0400

    New idea
    I'm going to use substrings instead

--- src/index.js
+++ src/index.js
@@ -1,32 +1,25 @@
-function simplify(poly){
+function simplify(poly) {
   //your code here
   //does not work
-//   console.log(poly.split("+"||"-");
-let str = poly;
-let string2 = str.replace(/\W/g, " ");
-// console.log(string2);
-let arr = string2.split(" ");
-console.log(arr);
-let plusArr = [];
-let minusArr = [];
-for(let i =0; i<str.length; i++){
-if(str.charAt(i)==="+"){
-plusArr.push(i)
-}
-if(str.charAt(i)==="-"){
-  minusArr.push(i)
-}
-}
-console.log(plusArr)
-console.log(minusArr)
+  //   console.log(poly.split("+"||"-");
+  let str = poly;
+
+  let plusOrMinusArr = [];
+  for (let i = 0; i < str.length; i++) {
+    if (str.charAt(i) === "+" || str.charAt(i) === "-") {
+      plusOrMinusArr.push(i);
+    }
+  }
+  console.log(str);
+  console.log(plusOrMinusArr);
 }
 
 simplify("dc+dcba"); //, "cd+abcd")
-simplify("2xy-yx") //,"xy")
-simplify("-a+5ab+3a-c-2a") //,"-c+5ab")
-simplify("-abc+3a+2ac") //,"3a+2ac-abc")
-simplify("xyz-xz") //,"-xz+xyz")
-simplify("a+ca-ab") //,"a-ab+ac")
-simplify("xzy+zby") //,"byz+xyz")
-simplify("-y+x") //,"x-y")
-simplify("y-x") //,"-x+y")
+simplify("2xy-yx"); //,"xy")
+simplify("-a+5ab+3a-c-2a"); //,"-c+5ab")
+simplify("-abc+3a+2ac"); //,"3a+2ac-abc")
+simplify("xyz-xz"); //,"-xz+xyz")
+simplify("a+ca-ab"); //,"a-ab+ac")
+simplify("xzy+zby"); //,"byz+xyz")
+simplify("-y+x"); //,"x-y")
+simplify("y-x"); //,"-x+y")
